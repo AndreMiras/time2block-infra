0fb4a5cbebb356be7a2fee056487f2b123089f8b
Andre Miras <andre.miras@gmail.com>
2024-12-21 12:38:48 +0100

    :sparkles: Cloudflare -> Cloud Run domain mapping
    Mapping the www.t2b.is domain to the Cloud Run resrouce
    Domain verified via:
    https://www.google.com/webmasters/verification/verification?domain=t2b.is
    Then add the service account to the autorized owners via:
    https://search.google.com/search-console/users?resource_id=sc-domain%3At2b.is
    
    Also gave the Cloudflare token `Page Rules:Edit` permissions.

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -4,6 +4,13 @@ resource "google_project_service" "secretmanager" {
   disable_on_destroy = false
 }
 
+# generated from https://dash.cloudflare.com/profile/api-tokens
+data "google_secret_manager_secret_version" "cloudflare_api_token" {
+  secret     = "${var.service_name}-cloudflare-api-token"
+  version    = "latest"
+  depends_on = [google_project_service.secretmanager]
+}
+
 data "google_secret_manager_secret" "chains" {
   secret_id  = "${var.service_name}-chains"
   depends_on = [google_project_service.secretmanager]
